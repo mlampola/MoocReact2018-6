ab23e08e49c6b85f6c11165954d34f78c9bb212a
Markus Lampola <markus.lampola@opr-finance.fi>
2018-12-07 15:07:27 +0200

    6.17 routed anecdotes, osa 2

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -9,11 +9,21 @@ const Menu = () => (
   </div>
 )
 
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+  </div>
+)
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 )
@@ -86,7 +96,6 @@ class CreateNew extends React.Component {
         </form>
       </div>
     )
-
   }
 }
 
@@ -146,6 +155,9 @@ class App extends React.Component {
             <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path="/about" render={() => <About />} />
             <Route path="/create" render={() => <CreateNew addNew={this.addNew} />} />
+            <Route exact path="/anecdotes/:id" render={({ match }) =>
+              <Anecdote anecdote={this.anecdoteById(match.params.id)} />} 
+            />
             <Footer />
           </div>
         </Router>
